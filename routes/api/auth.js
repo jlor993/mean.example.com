6f24bef4f3f2f80874b4e6959ad062c45153f2d5
Jeffrey Lor <jlor993@gmail.com>
2021-09-29 09:57:28 -0500

    Add logout methods

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -41,7 +41,22 @@ router.post('/login', function(req, res, next) {
     });
   })(req, res, next);
 });
+
+/*
 router.get('/logout', function(req, res){
+  console.log(req.session);
   req.logout();
+  console.log(req.session);
 });
+*/
+
+router.delete('/logout', function(req, res){
+  req.logout();
+  if(!req.session.passport.user){
+    return res.json({success: 'true'});
+  }else{
+    return res.json({success: 'false'});
+  }
+});
+
 module.exports = router;
